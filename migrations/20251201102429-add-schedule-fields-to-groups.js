47a0cd0d01ee3085718d86d2127a57d3fac0c94d
shuxratbek99 <shuhratbekzokirjonov1119@gmail.com>
2025-12-08 20:56:46 +0500

    secondss 15

--- migrations/20251201102429-add-schedule-fields-to-groups.js
+++ migrations/20251201102429-add-schedule-fields-to-groups.js
@@ -4,10 +4,10 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // Check if columns exist before adding
-    const tableInfo = await queryInterface.describeTable("Groups");
+    const tableInfo = await queryInterface.describeTable("groups");
 
     if (!tableInfo.schedule_days) {
-      await queryInterface.addColumn("Groups", "schedule_days", {
+      await queryInterface.addColumn("groups", "schedule_days", {
         type: Sequelize.JSON,
         defaultValue: JSON.stringify(["monday", "wednesday", "friday"]),
         allowNull: true,
@@ -15,7 +15,7 @@ module.exports = {
     }
 
     if (!tableInfo.schedule_time) {
-      await queryInterface.addColumn("Groups", "schedule_time", {
+      await queryInterface.addColumn("groups", "schedule_time", {
         type: Sequelize.TIME,
         defaultValue: "09:00:00",
         allowNull: true,
@@ -23,7 +23,7 @@ module.exports = {
     }
 
     if (!tableInfo.description) {
-      await queryInterface.addColumn("Groups", "description", {
+      await queryInterface.addColumn("groups", "description", {
         type: Sequelize.TEXT,
         allowNull: true,
       });
@@ -31,8 +31,8 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn("Groups", "schedule_days");
-    await queryInterface.removeColumn("Groups", "schedule_time");
-    await queryInterface.removeColumn("Groups", "description");
+    await queryInterface.removeColumn("groups", "schedule_days");
+    await queryInterface.removeColumn("groups", "schedule_time");
+    await queryInterface.removeColumn("groups", "description");
   },
 };
