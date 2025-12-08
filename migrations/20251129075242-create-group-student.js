de64e4b9890c327933ec5f23fb54a41cb78c5dde
shuxratbek99 <shuhratbekzokirjonov1119@gmail.com>
2025-12-08 15:39:59 +0500

    secondss 5

--- migrations/20251129075242-create-group-student.js
+++ migrations/20251129075242-create-group-student.js
@@ -14,7 +14,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "groups",
+          model: "Groups",
           key: "id",
         },
         onUpdate: "CASCADE",
