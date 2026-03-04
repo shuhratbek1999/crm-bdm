9c02a2355614d02364d9087acb7b84f823d38292
shuxratbek99 <shuhratbekzokirjonov1119@gmail.com>
2026-03-04 16:03:56 +0500

    fishedd

--- models/group.js
+++ models/group.js
@@ -371,7 +371,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "Group",
-      tableName: "groups",
+      tableName: "`groups`",
       underscored: true,
       timestamps: true,
       createdAt: "created_at",
